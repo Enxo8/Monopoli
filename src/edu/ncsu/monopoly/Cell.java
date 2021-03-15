13519678694e717c55063bcddeddc2bb43d8d0e5
eNZO <eNZO@DESKTOP-SPR2SPE>
2021-03-15 20:39:24 +0100

    Terminado

---
+++ src/edu/ncsu/monopoly/Cell.java
@@ -0,0 +1,41 @@
+package edu.ncsu.monopoly;
+
+public abstract class Cell implements IOwnable {
+	private boolean available = true;
+	private String name;
+	protected Player propietary;
+
+	public String getName() {
+		return name;
+	}
+
+	public Player getPropietary() {
+		return propietary;
+	}
+	
+	public int getPrice() {
+		return 0;
+	}
+
+	public boolean isAvailable() {
+		return available;
+	}
+	
+	public abstract boolean playAction(String msg);
+
+	public void setAvailable(boolean available) {
+		this.available = available;
+	}
+	
+	void setName(String name) {
+		this.name = name;
+	}
+
+	public void setPropietary(Player owner) {
+		this.propietary = owner;
+	}
+    
+    public String toString() {
+        return name;
+    }
+}
---
+++ src/edu/ncsu/monopoly/UtilityCell.java
@@ -0,0 +1,37 @@
+package edu.ncsu.monopoly;
+
+public class UtilityCell extends Cell {
+
+	public static final String COLOR_GROUP = "UTILITY";
+	private static int PRICE;
+
+	public static void setPrice(int price) {
+		UtilityCell.PRICE = price;
+	}
+
+	public int getPrice() {
+		return UtilityCell.PRICE;
+	}
+
+	public int getRent(int diceRoll) {
+		if(propietary.numberOfUtil() == 1) {
+			return diceRoll * 4;
+		} else if (propietary.numberOfUtil() >= 2) {
+			return diceRoll * 10;
+		}
+		return 0;
+	}
+
+	public boolean playAction(String msg) {
+		Player currentPlayer = null;
+		if(!isAvailable()) {
+			currentPlayer = GameMaster.instance().getCurrentPlayer();
+			if(propietary != currentPlayer) {
+				GameMaster.instance().utilRollDice();
+				int diceRoll = GameMaster.instance().getUtilDiceRoll();
+				currentPlayer.payRentTo(propietary, getRent(diceRoll));
+			}
+		}
+		return true;
+	}
+}
